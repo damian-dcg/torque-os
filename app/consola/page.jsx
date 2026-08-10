512f0b82d6c82b30b4ac172b64e5664caf12272a
damian-dcg <damian.e.carrasco@gmail.com>
2026-08-09 20:41:22 -0400

    Update page.jsx

--- app/consola/page.jsx
+++ app/consola/page.jsx
@@ -170,6 +170,7 @@ export default function Consola(){
         {tab==='checklists'&&<ModChecklists avisar={avisar}/>}
         {tab==='red'&&<ModRed avisar={avisar}/>}
         {tab==='reportes'&&<ModReportes/>}
+        {tab==='catalogos'&&<ModCatalogos avisar={avisar}/>}
         {tab==='config'&&<ModConfig tenant={tenant} avisar={avisar} onTenant={setTenant}/>}
       </div>
 
