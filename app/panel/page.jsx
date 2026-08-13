a4807cce70d276d5fa4a2fae60e9af5204cbd8d9
damian-dcg <damian.e.carrasco@gmail.com>
2026-08-13 16:54:07 -0400

    Delete app/panel directory

--- app/panel/page.jsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-export default function PanelRedirect(){
-  const r=useRouter();
-  useEffect(()=>{ r.replace('/consola'); },[]);
-  return null;
-}
